ea73eedb03ed2f106b043ec3692e378aeada7a7e
YPcrystal <azzahroshoffyah@gmail.com>
2024-12-31 08:22:45 +0700

    siswa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,3 +9,4 @@ Route::get('/', function () {
 
 //route resource for products
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
+Route::resource('/siswas', SiswaController::class);
