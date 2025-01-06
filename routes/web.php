9432ff92f993dc5c6bdd0f444eddbbc98f0893f2
YPcrystal <azzahroshoffyah@gmail.com>
2025-01-06 11:14:07 +0700

    laravel

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 //route resource for products
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
-Route::resource('/siswas', \App\Http\Controllers\SiswaController::class);
 Route::resource('/reporters', \App\Http\Controllers\ReporterController::class);
+Route::resource('/comments', \App\Http\Controllers\CommentController::class);
+Route::resource('/siswas', \App\Http\Controllers\SiswaController::class);
 Route::resource('/nilais', \App\Http\Controllers\NilaiController::class);
