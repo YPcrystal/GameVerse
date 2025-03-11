fc29955ab38b25d824f01751dd4bbff6516573a0
YPcrystal <azzahroshoffyah@gmail.com>
2025-03-11 10:56:16 +0700

    revisi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Game;
 
-class UserController extends Controller
+class GameController extends Controller
 {
     public function index()
     {
-        return view('user.dashboard');
+        $games = Game::all();
+        return view('games.index', compact('games'));
+    }
+
+    public function show(Game $game)
+    {
+        return view('games.show', compact('game'));
     }
 }
